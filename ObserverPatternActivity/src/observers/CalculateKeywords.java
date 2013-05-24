5cbd0108455fc05f39005e83818dc7a43cbf05f0
Daniel <Daniel@Dan>
2013-05-24 16:27:23 -0500

    Commit by: Daniel Malaver keywords

--- ObserverPatternActivity/src/observers/CalculateKeywords.java
+++ ObserverPatternActivity/src/observers/CalculateKeywords.java
@@ -20,4 +20,26 @@ public class CalculateKeywords implements Observer{
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
+    public void searchKeywords(Object o1){
+        int temp=0;
+        String[] keywords = {"Abstract","assert",
+        "Boolean", "break",
+        "byte", "case", "catch", "char", "class", "const", "continue", 
+        "default", "do", "double", "else", "enum", "extends", "final", 
+        "finally", "float", "for", "goto", "if", "implements", "import",
+        "instanceof", "int", "interface", "long", "native","new", "package","private", "protected", "public", 
+        "return", "short", "static", "strictfp", "super", "switch", "synchronized", "this", "throw", "throws",
+        "transient", "try","void", "volatile", "while"};
+        
+        for(int i=0;i<keywords.length;i++){
+        if(o1.equals(keywords)){
+            temp++;        
+         }
+        }
+        
+        System.out.println("keywords: "+temp);
+    
+    
+    }
 }
+
