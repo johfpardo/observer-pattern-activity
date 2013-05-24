d189fa3f4754c6e59f2a8f35c298e331b15a120a
Andres <Andres@Direccion-VAIO>
2013-05-24 16:08:20 -0500

    Commit by: Andr?s Guerrero Changes: implemented CalculateCharacters

--- ObserverPatternActivity/src/observers/CalculateCharacters.java
+++ ObserverPatternActivity/src/observers/CalculateCharacters.java
@@ -16,7 +16,7 @@ public class CalculateCharacters implements Observer{
     
     @Override
     public void update(Observable o, Object o1) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String cadena =(String)o1;                
+        System.out.println("Caracteres = " + cadena.length());
     }
-    
 }
